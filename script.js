8d30e69c2fdf5ba3b033ae1a16cdb76c171fac17
Mark Nesovanovic <mark.nesovanovic@gmail.com>
2025-07-20 22:06:38 +0200

    added basic js functions for operators

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(x,y) {
+    return x+y;
+}
+function subtract(x,y) {
+    return x-y;
+}
+function multiply(x,y) {
+    return x*y;
+}
+function divide(x,y) {
+    return x/y;
+}
+
+function operate(operator,x,y) {
+    let result = 0;
+    switch(operator) {
+        case '+':{
+            result = add(x,y);
+            break;
+        }
+        case '-': {
+            result = subtract(x,y);
+            break;
+        }
+        case '*': {
+            result = multiply(x,y);
+            break;
+        }
+        case '/': {
+            result = divide(x,y);
+            break;
+        }
+    }
+    return result;
+}
